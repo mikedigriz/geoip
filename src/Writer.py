55263198006a7a8a0b574d3ad270002bbd346f44
mikedigriz <mik.gryzunov@gmail.com>
2023-08-16 21:59:11 +0300

    fix naming

--- src/Writer.py
+++ src/Writer.py
@@ -6,6 +6,6 @@ def csv_writer(ip: str, country: str) -> None:
     Create if not exist and append file with data.
     If you use LibreOffice change encoding to Windows-1251.
     '''
-    with open('./proxy_by_country.csv', 'a', encoding='Windows-1251',  newline='') as f:
+    with open('./ip_by_country.csv', 'a', encoding='Windows-1251',  newline='') as f:
         writer = csv.writer(f, delimiter=';')
         writer.writerow([ip, country])
